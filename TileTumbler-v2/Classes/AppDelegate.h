2475ffabf0a847f23f143886905e283192cdbad8
Ronan Turner <ronan.a.turner@gmail.com>
2014-08-11 14:43:03 +0100

    Initial Commit

---
+++ TileTumbler-v2/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  TileTumbler-v2
+//
+//  Created by Ronan Turner on 11/08/2014.
+//  Copyright Ronan Turner 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ TileTumbler-v2/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TileTumbler-v2
+//
+//  Created by Ronan Turner on 11/08/2014.
+//  Copyright Ronan Turner 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
