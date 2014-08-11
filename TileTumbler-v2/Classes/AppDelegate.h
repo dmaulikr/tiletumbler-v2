53d845d2da4e0780b74c9f550c1ee60fe8c8afa4
Ronan Turner <ronan.a.turner@gmail.com>
2014-08-11 14:45:36 +0100

    Base commit.

--- TileTumbler-v2/Classes/AppDelegate.h
+++ TileTumbler-v2/Classes/AppDelegate.h
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.h
-//  TileTumbler-v2
-//
-//  Created by Ronan Turner on 11/08/2014.
-//  Copyright Ronan Turner 2014. All rights reserved.
-//
-// -----------------------------------------------------------------------
 
 #import "cocos2d.h"
 
